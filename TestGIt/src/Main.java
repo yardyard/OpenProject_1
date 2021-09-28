fd3e14ec27a8ff8db5b12e9e71f23c0704bb7c37
yardyard <84224418+yardyard@users.noreply.github.com>
2021-09-28 18:26:32 +0900

    첫 프로젝트

--- TestGIt/src/Main.java
+++ TestGIt/src/Main.java
@@ -5,23 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// 변수 선언하기
-		int firstNo, secondNo;
-		int sum;
-		
-		// 두 수 입력하기 : 라이브러리 사용해서 입력하기
-		Scanner scan = new Scanner(System.in);
-		System.out.print("첫수 : ");
-		firstNo = scan.nextInt();
-		System.out.print("두번쨰 수 : ");
-		secondNo = scan.nextInt();
-		
-		// 합계내기(C = A+B)
-		sum = firstNo + secondNo;
-		
-		// 합계 출력하기
-		System.out.println("합계 : " + sum);
-		System.out.println("첫 프로젝트");
+		System.out.println("Hello Repository");
 		
 	}
 
