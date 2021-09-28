384def297e5928101e74cc0a9d1f9c06917b807d
yardyard <84224418+yardyard@users.noreply.github.com>
2021-09-28 17:55:23 +0900

    2차 변경

--- TestGIt/src/Main.java
+++ TestGIt/src/Main.java
@@ -4,6 +4,21 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// 변수 선언하기
+		int firstNo, secondNo;
+		int sum;
+		
+		// 두 수 입력하기
+		firstNo = 10;
+		secondNo = 20;
+		
+		// 합계내기(C = A+B)
+		sum = firstNo + secondNo;
+		
+		// 합계 출력하기
+		System.out.println("합계 : " + sum);
+		
+		
 	}
 
 }
